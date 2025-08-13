1d1d70de87e05f4e61c8d1623ebc8fcea2c4200b
ANDRIUX7 <80667506+ANDRIUX7@users.noreply.github.com>
2025-08-13 00:32:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "ANDRIUX"
+    return "ONLINE 🟢"
 
 def run():
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
@@ -51,6 +51,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
